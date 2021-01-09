e3978cb5abdd74271662451c79b4d0c2719a2a06
Oyeleke Afolarin <oyelekeafolarin@Oyelekes-MacBook-Pro.local>
2021-01-09 16:59:46 +0100

    Added a weather app feature

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const request = require("request");
 
 const app = express();
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static("public"))
 
 
@@ -16,15 +16,13 @@ app.post("/", (req, res) => {
     const email = req.body.email;
 
     const data = {
-        members: [
-            {
-                email_address: email,
-                status: "subscribed",
-                merge_fields: {
-                    FNAME: name
-                }
+        members: [{
+            email_address: email,
+            status: "subscribed",
+            merge_fields: {
+                FNAME: name
             }
-        ]
+        }]
     }
 
     const jsonData = JSON.stringify(data)
@@ -38,8 +36,7 @@ app.post("/", (req, res) => {
     const request = https.request(url, options, (response) => {
         if (response.statusCode === 200) {
             res.sendFile(__dirname + "/success.html")
-        }
-        else {
+        } else {
             res.sendFile(__dirname + "/failure.html")
         }
         response.on("data", (data) => {
@@ -51,14 +48,35 @@ app.post("/", (req, res) => {
     request.end()
 })
 
-app.post("/failure.html", (req,res) => {
-    res.redirect("/")
-})
-app.post("/success.html", (req,res) => {
+app.post("/failure.html", (req, res) => {
     res.redirect("/")
 })
 app.listen(process.env.PORT || 3000, () => {
     console.log("Running on port 3000")
+})
+
+
+app.post("/success.html", (req, res) => {
+    const city = req.body.cityName;
+    const apiKey = "16787786e117b934880f5ca3c5c07ea4"
+    const unit = "metric"
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`
+    https.get(url, (response) => {
+        console.log(response.statusCode);
+        response.on("data", (data) => {
+            const weatherData = JSON.parse(data);
+            const temp = weatherData.main.temp;
+            const weatherDescription = weatherData.weather[0].description;
+            const icon = weatherData.weather[0].icon
+            const imgUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`
+            console.log(temp);
+            console.log(weatherDescription);
+            res.write(`<h1>The temperature in ${city} is ${temp} degrees Celcuis</h1>`)
+            res.write(`<h2>The weather is currently ${weatherDescription}</h2>`);
+            res.write(`<img src=${imgUrl}>`)
+            res.send();
+        })
+    })
 })
 
 // 7144460d697919e9587c35fd26f38a13-us7
---
+++ public/main.js
@@ -0,0 +1,10 @@
+var myModal = document.getElementById('myModal')
+var myInput = document.getElementById('myInput')
+
+
+// $(document).ready(function() {
+
+// });
+$('#trigger').click(function() {
+    $("#myModal").modal();
+});
